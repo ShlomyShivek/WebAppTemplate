d06e34764bdd86b359e9c6eac5c082eefc707f28
ShlomyShivek <shlomyshivek@gmail.com>
2014-11-19 21:46:26 +0200

    improve client side scripts

--- app/scripts/controllers/readmeCtrl.js
+++ app/scripts/controllers/readmeCtrl.js
@@ -1,6 +1,6 @@
 define(['../app', '../services/nodeService'],function(myApp, serviceClient){
     myApp.controller('ReadMeCtrl',['$scope','$http',function controller($scope, $http){
-        $scope.readmeData=serviceClient.getReadMeDataFromServer($http, onSuccessGetReadMeData, onFailureGetReadmeData);
+        serviceClient.getReadMeDataFromServer($http, onSuccessGetReadMeData, onFailureGetReadmeData);
 
         function onSuccessGetReadMeData(data){
             $scope.readmeData=data.message;
--- app/scripts/services/nodeService.js
+++ app/scripts/services/nodeService.js
@@ -1,17 +1,9 @@
 define([], function(){
     return{
         getTestDataFromServer: function(http, onSuccess, onFailure){
-            http.get('http://127.0.0.1:8081/api').
-                success(function(data, status, headers, config) {
-                    // this callback will be called asynchronously
-                    // when the response is available
-                    onSuccess(data);
-                }).
-                error(function(data, status, headers, config) {
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
-                    onFailure(data);
-                });
+            http.get('http://127.0.0.1:8081/api')
+                .success(onSuccess)
+                .error(onFailure);
         },
 
         getReadMeDataFromServer:function(http, onSuccess, onFailure){
