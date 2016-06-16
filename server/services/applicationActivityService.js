630a1ea6c196d0264262eef7c71e514299050484
shlomy.shivek <australia123>
2016-06-16 21:18:16 +0300

    TypeScript

--- server/services/applicationActivityService.js
+++ server/services/applicationActivityService.js
@@ -1,10 +1,17 @@
-exports.applicationStarted=function(){
-    var appActivityModel = require('dbModelsInitiator').getDbModel('appActivityLog');
-    var activity = new appActivityModel({date:'01/01/01', activity:'Application Started'});
-    activity.save(function (err, model) {
-        if (err) {
-            return console.error(err);
-        }
-        console.log('New data entered to DB:' + activity.toString());
-    });
-}
+var ActivityLogService = (function () {
+    function ActivityLogService() {
+    }
+    ActivityLogService.prototype.applicationStarted = function () {
+        var appActivityModel = require('dbModelsInitiator').getDbModel('appActivityLog');
+        var activity = new appActivityModel({ date: '01/01/01', activity: 'Application Started' });
+        activity.save(function (err, model) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log('New data entered to DB:' + activity.toString());
+        });
+    };
+    return ActivityLogService;
+})();
+exports.ActivityLogService = ActivityLogService;
+//# sourceMappingURL=applicationActivityService.js.map
---
+++ server/services/applicationActivityService.ts
@@ -0,0 +1,12 @@
+export class ActivityLogService {
+   applicationStarted():void {
+        var appActivityModel = require('dbModelsInitiator').getDbModel('appActivityLog');
+        var activity = new appActivityModel({date: '01/01/01', activity: 'Application Started'});
+        activity.save(function (err, model) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log('New data entered to DB:' + activity.toString());
+        });
+    }
+}
